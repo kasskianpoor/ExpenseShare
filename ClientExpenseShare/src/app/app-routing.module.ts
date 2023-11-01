994457c1720f944995fb944cf40f25641386c276
kkianpoo <kiarash_kianpoor@sfu.ca>
2023-11-01 14:18:42 -0700

    ordering of files and folders

--- ClientExpenseShare/src/app/app-routing.module.ts
+++ ClientExpenseShare/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { LoginComponent } from './login/login/login.component';
-import { SignupComponent } from './signup/signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const paths = {
   dashboard: '',
--- ClientExpenseShare/src/app/app.module.ts
+++ ClientExpenseShare/src/app/app.module.ts
@@ -4,23 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './login/login/login.component';
-import { SignupComponent } from './signup/signup/signup.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
