2c8a956d00e3c83c1e8c30f7e54ce5c90c559318
kkianpoo <kiarash_kianpoor@sfu.ca>
2023-11-01 17:34:03 -0700

    nav well integrated

--- ClientExpenseShare/src/app/app-routing.module.ts
+++ ClientExpenseShare/src/app/app-routing.module.ts
@@ -5,16 +5,16 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 export const paths = {
-  dashboard: '',
+  dashboard: 'dashboard',
   login: 'login',
   signup: 'signup',
   404: '**',
 };
 
 const routes: Routes = [
-  { path: paths.dashboard, component: DashboardComponent },
-  { path: paths.login, component: LoginComponent },
-  { path: paths.signup, component: SignupComponent },
+  { path: paths.dashboard, component: DashboardComponent, pathMatch: 'full' },
+  { path: paths.login, component: LoginComponent, pathMatch: 'full' },
+  { path: paths.signup, component: SignupComponent, pathMatch: 'full' },
   { path: paths[404], component: DashboardComponent, pathMatch: 'full' },
 ];
 
